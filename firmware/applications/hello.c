68f99035d2a7eceb6d69f008a4f2fde0ec082376
Administrator <administrator@maclane.topos.io>
2011-08-11 00:37:29 +0100

    Forcing a commit to do a merge.

--- firmware/applications/hello.c
+++ firmware/applications/hello.c
@@ -2,7 +2,7 @@
 #include "basic/basic.h"
 #include "lcd/render.h"
 void main_hello(void) {
-    DoString(10,20,"Hello World");
+    DoString(10,20,"Hello World Dan1");
     lcdDisplay();
     while (1) {}
 }
