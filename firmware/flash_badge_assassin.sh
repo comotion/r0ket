c83987325af1a2451f4ec85a9217cc014eec6985
Fabian Barkhau <fabe@fabe-laptop.(none)>
2011-08-13 11:56:15 +0200

    rocket tag changed log file

--- firmware/flash_badge_assassin.sh
+++ firmware/flash_badge_assassin.sh
@@ -4,7 +4,7 @@ if [ "$#" = 1 ]; then
 
     pwgen > applications/tmp_uuid.h
     echo "const char* uuid = \"`cat applications/tmp_uuid.h`\";" > applications/uuid.h
-    echo "\"`cat applications/tmp_uuid.h`\"; \"$1\"" >> applications/log_uuid.h
+    echo "\"`cat applications/tmp_uuid.h`\"; \"$1\"" >> applications/uuid.log
 
     make clean
     make flash APP=rocket_tag
